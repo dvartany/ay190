875a6301d9fe257ed3427a4f56679697bd8f5de5
davidvartanyan <dvartany@caltech.edu>
2014-01-14 21:55:30 -0800

    py for prob 1

--- ws2/ws21.py
+++ ws2/ws21.py
@@ -1,24 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jan  9 10:33:41 2014
-
-@author: davidvartanyan
-"""
-
 import numpy as np
+from pylab import *
 
 x0=1
 x1=np.float32(1.0/3)
 thirteenthird= np.float32(13.0/3)
 fourthird=np.float32(4.0/3)
+onethird=np.float32(1.0/3)
 
 def u(n):
     if n==0: return x0
     if n==1: return x1
-    return thirteenthird*(u(n-1)) - fourthird*(u(n-2))
+    return np.float32(thirteenthird*(u(n-1)) - fourthird*(u(n-2)))
+
+#assign function to list
 x = []
 for n in range(16):
     x.extend([u(n)])
+    
+#convert list to array
+x1=np.asarray(x)
 
-print x 
+def f(n):
+    return np.float32((onethird)**n)
+y=[]
+for n in range (16):
+    y.extend([f(n)])
+x2=np.asarray(y)
 
+zabs=x2-x1
+zrel=(x2-x1)/x2
+print zabs
+print zrel
